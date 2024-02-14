b06903f8459bf0e9d088b9815aa19c13fe9bddeb
straszydlo <kamil.szydlo.ks@gmail.com>
2024-02-14 14:09:01 +0100

    Inlined the tagging definitions

--- build.sbt
+++ build.sbt
@@ -3,5 +3,3 @@ version := "0.0.1"
 scalaVersion := "3.3.1"
 name := "SoftwareMill Tagging buggy bug reproduction"
 
-libraryDependencies += "com.softwaremill.common" %% "tagging" % "2.3.4"
-
--- src/main/scala/tag/me/Main.scala
+++ src/main/scala/tag/me/Main.scala
@@ -1,8 +1,14 @@
 package tag.me
 
-import com.softwaremill.tagging.*
-
 trait Foo
 
 @main def main = (new Foo {}).taggedWith[Int]
+
+trait Tag[+U] extends Any { type Tag <: U }
+type @@[+T, +U] = T with Tag[U]
+type Tagged[+T, +U] = T with Tag[U]
+
+implicit class Tagger[T](val t: T) extends AnyVal {
+  @inline def taggedWith[U]: T @@ U = t.asInstanceOf[T @@ U]
+}
 
